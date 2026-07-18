1c4dd83724a05962150a42771256f4966f89fbcb
suzan-sheikh <mdsuzanskh@gmail.com>
2026-07-19 00:38:34 +0600

    profile first oparation

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -4,6 +4,11 @@ import { profileService } from "./profile.service";
 const createProfile = async (req: Request, res: Response) => {
   try {
     const result = await profileService.createProfileIntoDB(req.body);
+    res.status(201).json({
+      success: true,
+      message: "Profile Create Successfully",
+      data: result.rows[0],
+    });
   } catch (error: any) {
     res.status(500).json({
       success: false,
